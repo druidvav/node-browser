aac0a2edda72638c8c9802c4f022fbe267df7d72
Anton Vlasov <druidvav@gmail.com>
2023-07-15 04:06:49 +0400

    configuration improved

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-// noinspection JSUnusedGlobalSymbols
-
 "use strict";
 let Curl = require('node-libcurl').Curl;
 let CurlIpResolve = require('node-libcurl').CurlIpResolve;
 let Cookie = require('tough-cookie').Cookie;
 let CookieJar = require('tough-cookie').CookieJar;
 
-let DvBrowser = function (config) {
-    if (!config) config = { };
-    config.timeout = config.timeout ?? 15;
-    config.connectTimeout = config.connectTimeout ?? 5;
-    config.httpProxy = config.httpProxy ?? null;
-    config.httpProxyUserPwd = config.httpProxyUserPwd ?? null;
-    config.allowRedirect = config.allowRedirect ?? true;
-    config.userAgent = config.userAgent ?? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36';
-    config.accept = config.accept ?? 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-    config.acceptLanguage = config.acceptLanguage ?? 'ru,en-US,en;q=0.8,ru;q=0.6';
+let DvBrowser = function (_config) {
+    const config = Object.assign({}, {
+        timeout: 15,
+        connectTimeout: 5,
+        httpProxy: null,
+        httpProxyUserPwd: null,
+        allowRedirect: true,
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        acceptLanguage: 'ru,en-US,en;q=0.8,ru;q=0.6',
+        cacheControl: 'max-age=0',
+        connection: 'close',
+        proxyConnection: 'close',
+    }, _config);
 
-    let globalHeaders = [
-        'Cache-Control: max-age=0',
-        'Proxy-Connection: close',
-        'Expect: '  // remove "Expect: 100-continue" header
-    ];
-
-    let cookiejar = new CookieJar();
-    let referer = '';
+    const cookiejar = new CookieJar(null);
 
     this.getProxy = function () { return config.httpProxy; };
-    this.setProxy = function (host, creds) { config.httpProxy = host; config.httpProxyUserPwd = creds; };
-    this.setReferer = function (url) { referer = url; };
     this.setCookie = function (cookie, url, options) { cookiejar.setCookieSync(cookie, url, options); };
-    this.setTimeout = function (connect, read) { config.connectTimeout = connect; config.timeout = read; };
-    this.setAllowRedirect = function (allowRedirect) { config.allowRedirect = allowRedirect };
     this.getCookie = function (key, url) {
         let cookies = cookiejar.getCookiesSync(url);
         for (let cookie of cookies) {
@@ -62,14 +53,16 @@ let DvBrowser = function (config) {
     };
     this.request = function (method, options) { return processRequest(method, options); };
 
-    function processRequest(method, options) {
+    function processRequest(method, _options) {
         return new Promise((resolve, reject) => {
+            const options = Object.assign({}, config, _options);
+
             let responseHeaders = [ ];
             let responseStatus = 0;
             let responseBody = [ ];
             let callbackCalled = false;
 
-            let headers = JSON.parse(JSON.stringify(globalHeaders)); // Cloning object
+            let headers = [ ];
             let cookieString = cookiejar.getCookieStringSync(options.url);
             if (cookieString) {
                 headers.push('Cookie: ' + cookieString);
@@ -79,36 +72,35 @@ let DvBrowser = function (config) {
             } else if (method === 'POST') {
                 headers.push('Content-Type: application/x-www-form-urlencoded');
             }
-            headers.push('Connection: ' + (options['connection'] ?? 'close'));
-            headers.push('Accept: ' + (options['accept'] ?? config['accept']));
-            headers.push('Accept-Language: ' + (options['acceptLanguage'] ?? config['acceptLanguage']));
-            headers.push('User-Agent: ' + (options['userAgent'] ?? config['userAgent']));
-            if (options['referer'] || referer) {
-                headers.push('Referer: ' + (options['referer'] ?? referer));
-            }
-            if (!options['noDeflate']) {
-                headers.push('Accept-Encoding: gzip, deflate');
-            }
+            headers.push('Connection: ' + options['connection']);
+            headers.push('Accept: ' + options['accept']);
+            headers.push('Accept-Language: ' + options['acceptLanguage']);
+            headers.push('User-Agent: ' + options['userAgent']);
+            headers.push('Cache-Control: ' + options['cacheControl']);
+            headers.push('Proxy-Connection: ' + options['proxyConnection']);
+            if (options['referer']) headers.push('Referer: ' + options['referer']);
+            if (!options['noDeflate']) headers.push('Accept-Encoding: gzip, deflate');
             if (options['headers']) {
                 for (let i = 0; i < options['headers'].length; i++) {
                     headers.push(options['headers'][i]);
                 }
             }
+            headers.push('Expect: ');
 
             let startTime = new Date().getTime();
             let timeoutControl = setInterval(() => {
                 let curTime = new Date().getTime();
                 let execTime = Math.round((curTime - startTime) / 1000);
-                if (execTime > (config.timeout + config.connectTimeout) * 2) {
-                    console.log('timeout', options.url, curTime - startTime, config);
+                if (execTime > (options.timeout + options.connectTimeout) * 2) {
+                    console.log('timeout', options.url, curTime - startTime, options);
                     throw new DvBrowserError('Something went wrong and timeout expired');
                 }
             }, 500);
             let curl = new Curl();
             curl.setOpt(Curl.option.URL, options.url);
-            curl.setOpt(Curl.option.TIMEOUT, config.timeout);
-            curl.setOpt(Curl.option.CONNECTTIMEOUT, config.connectTimeout);
-            curl.setOpt(Curl.option.LOW_SPEED_TIME, config.timeout);
+            curl.setOpt(Curl.option.TIMEOUT, options.timeout);
+            curl.setOpt(Curl.option.CONNECTTIMEOUT, options.connectTimeout);
+            curl.setOpt(Curl.option.LOW_SPEED_TIME, options.timeout);
             curl.setOpt(Curl.option.LOW_SPEED_LIMIT, 512);
             curl.setOpt(Curl.option.NOPROGRESS, true);
             curl.setOpt(Curl.option.HTTPHEADER, headers);
@@ -118,16 +110,16 @@ let DvBrowser = function (config) {
                 curl.setOpt(Curl.option.POST, true);
                 curl.setOpt(Curl.option.POSTFIELDS, options.postData);
             }
-            curl.setOpt(Curl.option.FOLLOWLOCATION, config.allowRedirect);
+            curl.setOpt(Curl.option.FOLLOWLOCATION, options.allowRedirect);
             curl.setOpt(Curl.option.SSL_VERIFYHOST, false);
             curl.setOpt(Curl.option.SSL_VERIFYPEER, false);
             if (!options['noDeflate']) {
                 curl.setOpt(Curl.option.ACCEPT_ENCODING, 'gzip');
             }
-            if (config.httpProxy) {
-                curl.setOpt(Curl.option.PROXY, config.httpProxy);
-                if (config.httpProxyUserPwd) {
-                    curl.setOpt(Curl.option.PROXYUSERPWD, config.httpProxyUserPwd);
+            if (options.httpProxy) {
+                curl.setOpt(Curl.option.PROXY, options.httpProxy);
+                if (options.httpProxyUserPwd) {
+                    curl.setOpt(Curl.option.PROXYUSERPWD, options.httpProxyUserPwd);
                 }
             }
             curl.on('header', handleHeader);
@@ -143,7 +135,7 @@ let DvBrowser = function (config) {
             function handleHeader(chunk) {
                 let header = chunk.toString().replace("\r", '').replace("\n", '');
                 if (chunk.length > 2) {
-                    if (header.substr(0, 4) === 'HTTP') {
+                    if (header.substring(0, 4) === 'HTTP') {
                         let status = header.split(' ');
                         responseStatus = status[1];
                     } else {
